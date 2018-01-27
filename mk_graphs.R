1bf3929e059c558a1fce4dc9acf4f70d9363ef02
Matthew Collins <mcollins@acis.ufl.edu>
2018-01-27 12:10:03 -0500

    Adjust mostly axes to improve graphs, add date stamp

--- mk_graphs.R
+++ mk_graphs.R
@@ -1,14 +1,21 @@
 library(tidyverse)
+library(stringi) # stri_pad_both()
+library(scales) # pretty_breaks()
 
 participants <- read.csv("data/MergedParticipants.csv", stringsAsFactors = FALSE)
+# Coding for students has varied over time, merge together some older codes and assume
+# that all "Student"s were graduate.
 corrected_participants <- participants %>%
   rename(rawAffiliation = Affiliation) %>%
-  mutate(Affiliation = ifelse(rawAffiliation == "Student", "Graduate", rawAffiliation))
+  mutate(Affiliation = ifelse(rawAffiliation == "Student" | rawAffiliation == "PhD"  | rawAffiliation == "Masters",
+                              "Graduate", rawAffiliation))
 
+# Subtitle for all graphs so we know when they were generated
+as_of = paste0("As of ", Sys.Date())
 
 affiliation_graph <- function(count_data, title="Participants by Affiliation"){
   # top to bottom ordering of affiliations to draw
-  affiliations <- c("Undergraduate", "Graduate", "Post-Doc", "Staff", "Faculty", "Other")
+  affiliations <- c("Undergraduate", "Graduate", "Post-Doc", "Staff", "Faculty", "Outside", "Unknown")
   
   # Y location for text
   text_y <- max(count_data$n) * 0.66
@@ -24,7 +31,7 @@ affiliation_graph <- function(count_data, title="Participants by Affiliation"){
   # and replace those other affiliation rows with an "Other" total
   plot_data <- count_data %>%
     filter(Affiliation %in% affiliations) %>%
-    union(data.frame(Affiliation="Other", n=others$nn))
+    union(data.frame(Affiliation="Unknown", n=others$nn))
 
   # set bar order by re-assigning the factor levels, note reversing  
   plot_data$Affiliation = factor(plot_data$Affiliation, levels=rev(affiliations))
@@ -33,8 +40,10 @@ affiliation_graph <- function(count_data, title="Participants by Affiliation"){
     geom_bar(stat="identity") +
     coord_flip() +
     guides(fill=FALSE) +
-    theme(plot.title = element_text(hjust = 0.5)) + 
-    labs(y = "Number of Participants", x = "UF Affiliation", title=title) + 
+    scale_y_continuous(limits=c(0, NA), breaks=pretty_breaks()) +
+    theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5)) + 
+    labs(y = "Number of Participants", x = "UF Affiliation", title=title,
+         subtitle=as_of) + 
     ggplot2::annotate("text", y=text_y, x=1.5, label=paste0("Total Participants: ", everyone$nn))
     # tm library has an "annotate" too
   return(p)
@@ -76,6 +85,11 @@ v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
 set.seed(8)
 png(paste0("graphs/wordcloud_", "all", ".png"), height=450, width=600)
+layout(matrix(c(1, 2), nrow=2), heights=c(1, 4))
+par(mar=rep(0, 4))
+plot.new()
+text(x=0.5, y=0.5, cex=2, "Wordcloud of Participant Departments")
+text(x=0.5, y=0.1, cex=1.6, as_of)
 wordcloud(words=d$word, freq=d$freq, 
           random.order=TRUE, max.words=50, colors=brewer.pal(8, "Dark2"))
 dev.off()
@@ -89,23 +103,27 @@ participants_over_time <- corrected_participants %>%
   arrange(Date) %>%
   mutate(number = row_number())
 
-year_starts <- as.Date(paste(unique(corrected_participants$Year), 1, 1, sep='-'))
+year_starts <- data.frame(intercept=
+                            as.Date(paste(unique(corrected_participants$Year), 1, 1, sep='-'))
+)
 
 cumulative_graph <- function(cumulative_data, y, title){
-  p <- ggplot(cumulative_data, aes(x=Date, y=cumsum(y))) + 
+  y_data = cumsum(y)
+  
+  p <- ggplot(cumulative_data, aes(x=Date, y=y_data)) + 
     geom_step(direction="hv", colour="dodgerblue", size=1) + 
     scale_x_date(breaks=cumulative_data$Date,
-                 labels=paste0(cumulative_data$Carpentry, "\n", cumulative_data$Date),
-                 limits=c(as.Date("2016-03-15"), NA)) +
-    scale_y_continuous(limits=c(0, NA)) + 
+                 labels=paste0(stri_pad_both(cumulative_data$Carpentry, width=10),
+                               "\n", cumulative_data$Date),
+                 limits=c(as.Date("2016-01-20"), NA)) +
+    scale_y_continuous(limits=c(0, NA), breaks=pretty_breaks()) + 
     labs(y = "Cumulative Number", x = "Workshops", title=title) + 
-    theme(plot.title = element_text(hjust = 0.5)) + 
-    theme(axis.text.x= element_text(angle=35, hjust = 1))
+    theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5)) +
+    theme(axis.text.x=element_text(size=5, angle=90, hjust=0.5, vjust=0.5)) + 
+    theme(axis.text.y=element_text(size=5))
   
-  for (x in year_starts){
-    p <- p + geom_vline(aes(xintercept=x), colour="indianred", linetype="dashed", size=0.5)
-  }
-  
+  p <- p + geom_vline(data=year_starts, aes(xintercept=intercept), colour="indianred", linetype="dashed", size=0.5)
+
   return(p)
 }
 
