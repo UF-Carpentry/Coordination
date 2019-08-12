b42464a291eaa9c266425812a79ece10c49970d2
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-08-12 14:25:35 -0400

    Lowercased and trimmed e-mails, wrote out outputs.

--- travel_awards/travel_awards.R
+++ travel_awards/travel_awards.R
@@ -11,6 +11,9 @@ library(tidyverse)
 # Download the 'instructors' file from the Google Drive as a CSV.
 inst_df <- read.csv("data/instructors.csv", header=TRUE)
 
+# Lowercase e-mail address to avoid case-related issues.
+inst_df <- inst_df %>% mutate(email_lc = trimws(tolower(Email)))
+
 print("Have data for these events:")
 inst_df %>%
   distinct(event, date) %>%
@@ -22,14 +25,15 @@ since_cutoff %>% distinct(event, date) %>% arrange(event, date)
 
 print("Crosstab of how many times people have done what in 2 years")
 whos_done_what <- since_cutoff %>%
-  group_by(Name, Email, role, event) %>%
+  group_by(Name, email_lc, role, event) %>%
   arrange(Name, role)
 whos_done_what %>% print(n=1000)
+whos_done_what %>% write_csv('data/whos_done_what.csv')
 
 print("Who has contributed (in any role) more than twice since cutoff_date")
 selected = whos_done_what %>%
-  group_by(Name, Email) %>%
+  group_by(Name, email_lc) %>%
   count() %>%
   filter(n>=2)
 selected %>% print(n=1000)
-
+selected %>% write_csv('data/travel_award_candidates.csv')
